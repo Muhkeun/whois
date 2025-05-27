6f54052c74e142cb9efce18fa56b97c3687ef4fb
muhkeun <muhkeun@naver.com>
2025-05-27 17:47:12 +0900

    init

---
+++ llm/src/test/java/com/muhkeun/whois/llm/LlmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.muhkeun.whois.llm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LlmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
